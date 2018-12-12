8792073048dd84b911856b34487be8fc84079876
DJN1 <david.niederweis@gmail.com>
2018-12-12 13:00:11 -0600

    added files, connected front to backend

--- 3D Printing/app.py
+++ 3D Printing/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 import os
+from O365 import Message
 
 app = Flask(__name__)
 port = 4000
@@ -18,8 +19,11 @@ def page_not_found(e):
 
     return render_template('404.html'), 404
 
-
-
+@app.route("/placeOrder", methods=['POST', 'GET'])
+def placeOrder():
+    if request.method == 'POST':
+        print(request.files["file"] is not None)
+    return ('', 204)
 
 # os.system('cls')
 app.run(port=port)
