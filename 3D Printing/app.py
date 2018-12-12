dc46ba0ffb74e20a2a7a5427c7476f36336a4e7b
DJN1 <david.niederweis@gmail.com>
2018-12-12 14:41:01 -0600

    fixed authentication issue

--- 3D Printing/app.py
+++ 3D Printing/app.py
@@ -2,7 +2,20 @@ from flask import Flask, redirect, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 import os
-from O365 import Message
+from O365 import *
+
+O365_username = ""
+O365_password = ""
+
+O365_file = open('O365_auth.txt')
+lines = O365_file.readlines()
+O365_username = lines[0]
+O365_username = O365_username[0:len(O365_username) - 1]
+O365_password = lines[1]
+
+
+O365_creds = (O365_username, O365_password)
+
 
 app = Flask(__name__)
 port = 4000
@@ -22,7 +35,14 @@ def page_not_found(e):
 @app.route("/placeOrder", methods=['POST', 'GET'])
 def placeOrder():
     if request.method == 'POST':
-        print(request.files["file"] is not None)
+        print(request.files["file"])
+
+        m = Message(auth=O365_creds)
+        m.setRecipients("david.niederweis@gmail.com")
+        m.setSubject("scriptTest")
+        m.setBody("This is a test to see if this script works")
+        m.sendMessage()
+
     return ('', 204)
 
 # os.system('cls')
